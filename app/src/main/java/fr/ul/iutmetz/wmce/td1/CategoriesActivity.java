0a48c761735244599c1ac21087f640010ec70bd6
Selene VIOLA <selene.viola9@etu.univ-lorraine.fr>
2020-12-04 17:55:32 +0100

    Avancement bdd

--- app/src/main/java/fr/ul/iutmetz/wmce/td1/CategoriesActivity.java
+++ app/src/main/java/fr/ul/iutmetz/wmce/td1/CategoriesActivity.java
@@ -6,23 +6,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.VolleyError;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.util.ArrayList;
 
+import fr.ul.iutmetz.wmce.td1.DAO.CategorieDAO;
 import fr.ul.iutmetz.wmce.td1.modele.Categorie;
 import fr.ul.iutmetz.wmce.td1.modele.Produit;
 import utils.Utils;
 
 public class CategoriesActivity extends AppCompatActivity
-    implements AdapterView.OnItemClickListener, ActiviteEnAttenteImage {
+    implements AdapterView.OnItemClickListener, ActiviteEnAttenteImage,
+                com.android.volley.Response.Listener<JSONArray>,
+                com.android.volley.Response.ErrorListener {
 
     private static final int MAIN_VENTE = 0;
     private static final int MAIN_CATALOGUE = 1;
@@ -55,13 +66,16 @@ public class CategoriesActivity extends AppCompatActivity
             this.listeCategories = (ArrayList<Categorie>) savedInstanceState.getSerializable("listeCategorie");
             this.totalPanier = utils.arrondir(savedInstanceState.getDouble("total_panier"));
         } else {
-            Categorie c0 = new Categorie(0, "Pulls", "renne");
-            Categorie c1 = new Categorie(1, "Bonnets", "bonnet_renne");
-            Categorie c2 = new Categorie(2, "Casquettes", "casquette_dab");
+//            Categorie c0 = new Categorie(0, "Pulls", "renne");
+//            Categorie c1 = new Categorie(1, "Bonnets", "bonnet_renne");
+//            Categorie c2 = new Categorie(2, "Casquettes", "casquette_dab");
+//
+//            listeCategories.add(c0);
+//            listeCategories.add(c1);
+//            listeCategories.add(c2);
 
-            listeCategories.add(c0);
-            listeCategories.add(c1);
-            listeCategories.add(c2);
+            CategorieDAO catDAO = new CategorieDAO();
+            catDAO.findAll(this);
 
             this.totalPanier = utils.arrondir(0.00);
 
@@ -72,7 +86,7 @@ public class CategoriesActivity extends AppCompatActivity
             this.listeImagesCategories.add(null);
             ImageFromURL chargement = new ImageFromURL(this);
             chargement.execute("https://devweb.iutmetz.univ-lorraine.fr/~viola11u/WS_PM/" +
-                    this.listeCategories.get(i).getVisuel() + ".jpg", String.valueOf(this.listeCategories.get(i).getId()));
+                    this.listeCategories.get(i).getVisuel(), String.valueOf(this.listeCategories.get(i).getId()));
         }
     }
 
@@ -133,4 +147,26 @@ public class CategoriesActivity extends AppCompatActivity
         }
     }
 
+    @Override
+    public void onErrorResponse(VolleyError error) {
+        Log.e("Erreur JSON", error + "");
+        Toast.makeText(this, R.string.ca_erreur_bdd, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onResponse(JSONArray response) {
+
+        try {
+            for (int i = 0 ; i < response.length() ; i++){
+                JSONObject o = response.getJSONObject(i);
+                System.out.println("---------- cat " + i + "------------------");
+                int idCat = o.getInt("id_categorie");
+                String title = o.getString("titre");
+                String visuel = o.getString("visuel");
+                this.listeCategories.add(new Categorie(idCat, title, visuel));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
 }
