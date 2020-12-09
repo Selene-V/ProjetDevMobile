83f4b3a6eadb5ed7ebd8bc63fd2529103ccd8650
Selene VIOLA <selene.viola9@etu.univ-lorraine.fr>
2020-12-09 19:39:22 +0100

    Mise a jour bdd

--- sql/viola11u_projet_dev_mob.sql
+++ sql/viola11u_projet_dev_mob.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net
 --
 -- Client :  devbdd.iutmetz.univ-lorraine.fr
--- Généré le :  Mer 09 Décembre 2020 à 11:32
+-- Généré le :  Mer 09 Décembre 2020 à 19:35
 -- Version du serveur :  10.3.25-MariaDB
 -- Version de PHP :  7.1.33
 
@@ -44,6 +44,85 @@ INSERT INTO `Categorie` (`id_categorie`, `titre`, `visuel`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `Client`
+--
+
+CREATE TABLE IF NOT EXISTS `Client` (
+  `id_client` int(5) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `prenom` varchar(50) NOT NULL,
+  `identifiant` varchar(50) NOT NULL,
+  `mot_de_passe` varchar(255) NOT NULL,
+  `adr_numero` varchar(10) NOT NULL,
+  `adr_voie` varchar(255) NOT NULL,
+  `adr_code_postal` varchar(255) NOT NULL,
+  `adr_ville` varchar(255) NOT NULL,
+  `adr_pays` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Commande`
+--
+
+CREATE TABLE IF NOT EXISTS `Commande` (
+  `id_commande` int(5) NOT NULL,
+  `date_commande` date NOT NULL,
+  `id_client` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Favori`
+--
+
+CREATE TABLE IF NOT EXISTS `Favori` (
+  `id_client` int(5) NOT NULL,
+  `id_produit` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Ligne_commande`
+--
+
+CREATE TABLE IF NOT EXISTS `Ligne_commande` (
+  `id_commande` int(5) NOT NULL,
+  `id_produit` int(5) NOT NULL,
+  `id_taille` int(2) NOT NULL,
+  `quantite` int(5) NOT NULL,
+  `tarif` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Magasin`
+--
+
+CREATE TABLE IF NOT EXISTS `Magasin` (
+  `id_magasin` int(5) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `latitude` varchar(50) NOT NULL,
+  `longitude` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Mentions_legales`
+--
+
+CREATE TABLE IF NOT EXISTS `Mentions_legales` (
+  `mentions` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `Produit`
 --
 
@@ -61,20 +140,15 @@ CREATE TABLE IF NOT EXISTS `Produit` (
 --
 
 INSERT INTO `Produit` (`id_produit`, `titre`, `description`, `tarif`, `visuel`, `id_categorie`) VALUES
-(1, 'A Noël c''est moi qui tient les rennes', 'Un pull qui ravira les petits et les grands. Tricoté par d''authentiques grand-mères Australiennes, en laine 100% coton naturel issue d''une filière agriculture durable certifiée ISO-2560.', 52, 'pull0.jpg', 1),
-(2, 'Sonic te kiffe', 'Inspiré par la saga Séga (c''est plus fort que toi !), un pull 100% gamer qui te permettra de faire baver d''envie tes petits camarades de jeu.', 41.5, 'pull1.jpg', 1),
-(3, 'Mon renne a les boules', 'Un grand classique revisité, à la fois renne et sapin de Noël, c''est toi le plus beau quand tu es décoré par ce pull !', 44, 'pull2.jpg', 1),
-(4, 'Le père Noël a une gastro', 'Ah, le chic à la Française. Parce que les stars aussi vont sur le pot de temps en temps...', 35.2, 'pull3.jpg', 1),
-(5, 'Une guirlande de pères Noël', 'Ils sont tous en rang, ils te regardent, à toi d''être bien sage pour mériter ce pull de fête !', 38, 'pull4.jpg', 1),
-(6, 'La chaleur des rennes', 'Classique mais efficace, un bonnet dont l''élégance n''est pas à souligner, il vous grattera comme il faut !', 15, 'bonnet0.jpg', 2),
-(7, 'Noël Lorientais', 'Idéal pour tous les fans du FC Lorient, mais pas que !', 22, 'bonnet1.jpg', 2),
-(8, 'Beau comme un Elfe', 'HE bonnet à oreilles, couleurs indémodables pour cette création inspirée de l''univers de Tolkien.', 17, 'bonnet2.jpg', 2),
-(9, 'Traineau de rennes', 'Un grand classique, ce magnifique bonnet vous sierra en toutes circonstances, et s''adaptera à toutes vos tenues hivernales.', 15, 'bonnet3.jpg', 2),
-(10, 'Real Santa', 'En direct de NYC, avec bois de rennes télescopiques !!!', 20, 'bonnet4.jpg', 2),
-(11, 'Les trois amis', 'Un trio féérique prendra soin de votre calvitie naissante, vous en serez ravi !', 30, 'casquette0.jpg', 3),
-(12, 'Dall', 'Joyeux Noël avec nos petits lutins dansants !', 35, 'casquette1.jpg', 3),
-(13, 'Magie de Noël', 'Quoi de plus beau qu''un bonhomme de neige avec les enfants quand les premiers flocons tombent du ciel ?', 26, 'casquette2.jpg', 3),
-(14, 'Santa Hangover', 'Le Père Noël est bien fatigué sur cette magnifique casquette, mais n''est-ce pas notre lot à tous une fois les fêtes passées ?', 30, 'casquette3.jpg', 3);
+(1, 'A Noël c''est moi qui tient les rennes BDD', 'Un pull qui ravira les petits et les grands. Tricoté par d''authentiques grand-mères Australiennes, en laine 100% coton naturel issue d''une filière agriculture durable certifiée ISO-2560.', 52, 'renne.jpg', 1),
+(2, 'Chien de noël', 'Un pull qui te permettra de faire baver d''envie tes petits camarades.', 41.5, 'chien.jpg', 1),
+(3, 'Un lama au poil !', 'Un grand classique revisité, c''est toi le plus beau quand tu es décoré par ce pull !', 44, 'lama.jpg', 1),
+(4, 'Mon beau sapin !', 'Ah, le chic à la Française, notre roi des forêts !', 35.2, 'sapin.jpg', 1),
+(5, 'Un pull pour deux !', 'Pour des soirées au chaud en amoureux !', 38, 'double_pull.jpg', 1),
+(6, 'Bonnet en laine', 'Ceci est un magnifique bonnet en laine qui tient extrêmement chaud !', 15, 'bonnet_renne.jpg', 2),
+(7, 'Bonnet en laine plus cher', 'Ce bonnet est exactement le même que celui vu précédemment mais en plus cher !  Merci d''acheter celui la !', 22, 'bonnet_renne.jpg', 2),
+(8, 'Dabsquette', 'Voici une casquette très laide et qui ne tient absolument pas chaud mais il y a un père noël dessus.', 17, 'casquette_dab.jpg', 3),
+(9, 'Joli renne', 'Grâce à cette casquette vous pourrez vous faire passer pour un magnifique renne et tout le monde n''y verra que du feu !', 15, 'casquette_renne.jpg', 3);
 
 -- --------------------------------------------------------
 
@@ -123,27 +197,7 @@ INSERT INTO `Produit_taille` (`id_produit`, `id_taille`) VALUES
 (6, 7),
 (7, 7),
 (8, 7),
-(9, 7),
-(10, 7),
-(11, 8),
-(11, 9),
-(11, 10),
-(11, 11),
-(11, 12),
-(11, 13),
-(12, 9),
-(12, 11),
-(12, 13),
-(13, 8),
-(13, 9),
-(13, 10),
-(13, 11),
-(13, 12),
-(13, 13),
-(14, 8),
-(14, 10),
-(14, 11),
-(14, 13);
+(9, 7);
 
 -- --------------------------------------------------------
 
@@ -186,16 +240,53 @@ ALTER TABLE `Categorie`
   ADD PRIMARY KEY (`id_categorie`);
 
 --
+-- Index pour la table `Client`
+--
+ALTER TABLE `Client`
+  ADD PRIMARY KEY (`id_client`);
+
+--
+-- Index pour la table `Commande`
+--
+ALTER TABLE `Commande`
+  ADD PRIMARY KEY (`id_commande`),
+  ADD KEY `id_client` (`id_client`);
+
+--
+-- Index pour la table `Favori`
+--
+ALTER TABLE `Favori`
+  ADD PRIMARY KEY (`id_client`,`id_produit`),
+  ADD KEY `favoris_id_produit_fkey` (`id_produit`);
+
+--
+-- Index pour la table `Ligne_commande`
+--
+ALTER TABLE `Ligne_commande`
+  ADD KEY `id_commande` (`id_commande`,`id_produit`,`id_taille`),
+  ADD KEY `ligne_commande_id_produit_fkey` (`id_produit`),
+  ADD KEY `ligne_commande_id_taille_fkey` (`id_taille`);
+
+--
+-- Index pour la table `Magasin`
+--
+ALTER TABLE `Magasin`
+  ADD PRIMARY KEY (`id_magasin`);
+
+--
 -- Index pour la table `Produit`
 --
 ALTER TABLE `Produit`
-  ADD PRIMARY KEY (`id_produit`);
+  ADD PRIMARY KEY (`id_produit`),
+  ADD KEY `produit_id_categorie_fkey` (`id_categorie`);
 
 --
 -- Index pour la table `Produit_taille`
 --
 ALTER TABLE `Produit_taille`
-  ADD PRIMARY KEY (`id_produit`,`id_taille`);
+  ADD PRIMARY KEY (`id_produit`,`id_taille`),
+  ADD KEY `id_produit` (`id_produit`,`id_taille`),
+  ADD KEY `produit_taille_id_taille_fkey` (`id_taille`);
 
 --
 -- Index pour la table `Taille`
@@ -213,6 +304,21 @@ ALTER TABLE `Taille`
 ALTER TABLE `Categorie`
   MODIFY `id_categorie` int(3) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
+-- AUTO_INCREMENT pour la table `Client`
+--
+ALTER TABLE `Client`
+  MODIFY `id_client` int(5) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `Commande`
+--
+ALTER TABLE `Commande`
+  MODIFY `id_commande` int(5) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `Magasin`
+--
+ALTER TABLE `Magasin`
+  MODIFY `id_magasin` int(5) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT pour la table `Produit`
 --
 ALTER TABLE `Produit`
@@ -222,6 +328,44 @@ ALTER TABLE `Produit`
 --
 ALTER TABLE `Taille`
   MODIFY `id_taille` int(2) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `Commande`
+--
+ALTER TABLE `Commande`
+  ADD CONSTRAINT `commande_id_client_fkey` FOREIGN KEY (`id_client`) REFERENCES `Client` (`id_client`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `Favori`
+--
+ALTER TABLE `Favori`
+  ADD CONSTRAINT `favoris_id_client_fkey` FOREIGN KEY (`id_client`) REFERENCES `Client` (`id_client`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `favoris_id_produit_fkey` FOREIGN KEY (`id_produit`) REFERENCES `Produit` (`id_produit`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `Ligne_commande`
+--
+ALTER TABLE `Ligne_commande`
+  ADD CONSTRAINT `ligne_commande_id_commande_fkey` FOREIGN KEY (`id_commande`) REFERENCES `Commande` (`id_commande`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `ligne_commande_id_produit_fkey` FOREIGN KEY (`id_produit`) REFERENCES `Produit` (`id_produit`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `ligne_commande_id_taille_fkey` FOREIGN KEY (`id_taille`) REFERENCES `Taille` (`id_taille`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `Produit`
+--
+ALTER TABLE `Produit`
+  ADD CONSTRAINT `produit_id_categorie_fkey` FOREIGN KEY (`id_categorie`) REFERENCES `Categorie` (`id_categorie`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `Produit_taille`
+--
+ALTER TABLE `Produit_taille`
+  ADD CONSTRAINT `produit_taille_id_produit_fkey` FOREIGN KEY (`id_produit`) REFERENCES `Produit` (`id_produit`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `produit_taille_id_taille_fkey` FOREIGN KEY (`id_taille`) REFERENCES `Taille` (`id_taille`) ON DELETE CASCADE ON UPDATE CASCADE;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
