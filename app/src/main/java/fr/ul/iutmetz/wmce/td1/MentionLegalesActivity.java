6c33b08a76e6528be6560fd3fa017895cb96e1c6
Selene VIOLA <selene.viola9@etu.univ-lorraine.fr>
2020-12-14 14:59:09 +0100

    Ajout mentions legales

---
+++ app/src/main/java/fr/ul/iutmetz/wmce/td1/MentionLegalesActivity.java
@@ -0,0 +1,14 @@
+package fr.ul.iutmetz.wmce.td1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MentionLegalesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mention_legales);
+    }
+}
