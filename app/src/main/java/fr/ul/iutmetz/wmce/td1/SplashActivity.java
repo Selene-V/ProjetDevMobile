35bd46b9c26c501dff98bd4f65fc42a9c6f9da30
RFrancois6 <romain57890@orange.fr>
2020-12-14 10:53:33 +0100

    splasher + map

--- app/src/main/java/fr/ul/iutmetz/wmce/td1/SplashActivity.java
+++ app/src/main/java/fr/ul/iutmetz/wmce/td1/SplashActivity.java
@@ -18,10 +18,10 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashActivity.this, CategoriesActivity.class);
+                Intent intent = new Intent(SplashActivity.this, MapsActivity.class);
                 startActivity(intent);
                 finish();
             }
-        }, 10*1000);
+        }, 3*1000);
     }
 }
