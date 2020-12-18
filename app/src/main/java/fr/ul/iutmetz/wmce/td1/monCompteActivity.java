6457369dd60fbf11d4b6787b4799b6c9d9b4eb47
Selene VIOLA <selene.viola9@etu.univ-lorraine.fr>
2020-12-18 09:16:29 +0100

    Modif bug git + debut moncompte + add branch compte

---
+++ app/src/main/java/fr/ul/iutmetz/wmce/td1/monCompteActivity.java
@@ -0,0 +1,14 @@
+package fr.ul.iutmetz.wmce.td1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class monCompteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mon_compte);
+    }
+}
