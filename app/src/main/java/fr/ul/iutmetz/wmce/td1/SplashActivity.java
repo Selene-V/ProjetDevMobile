e8b8de4f302c3edaf6833d9d7a1a3e161499083a
Selene VIOLA <selene.viola9@etu.univ-lorraine.fr>
2021-01-24 13:43:41 +0100

    Debut commandes

--- app/src/main/java/fr/ul/iutmetz/wmce/td1/SplashActivity.java
+++ app/src/main/java/fr/ul/iutmetz/wmce/td1/SplashActivity.java
@@ -19,7 +19,7 @@ public class SplashActivity extends AppCompatActivity {
             @Override
             public void run() {
 //                Intent intent = new Intent(SplashActivity.this, ConnexionActivity.class);
-                Intent intent = new Intent(SplashActivity.this, MentionLegalesActivity.class);
+                Intent intent = new Intent(SplashActivity.this, CommandesActivity.class);
                 startActivity(intent);
                 finish();
             }
