fe4f9044b56b14d3c26e4c2a5526d60a729f14ef
Selene VIOLA <selene.viola9@etu.univ-lorraine.fr>
2021-01-29 09:33:32 +0100

    Maj BDD sql

--- sql/viola11u_projet_dev_mob.sql
+++ sql/viola11u_projet_dev_mob.sql
@@ -3,8 +3,8 @@
 -- https://www.phpmyadmin.net
 --
 -- Client :  devbdd.iutmetz.univ-lorraine.fr
--- Généré le :  Mer 09 Décembre 2020 à 19:35
--- Version du serveur :  10.3.25-MariaDB
+-- Généré le :  Ven 29 Janvier 2021 à 09:29
+-- Version du serveur :  10.3.27-MariaDB
 -- Version de PHP :  7.1.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -58,7 +58,15 @@ CREATE TABLE IF NOT EXISTS `Client` (
   `adr_code_postal` varchar(255) NOT NULL,
   `adr_ville` varchar(255) NOT NULL,
   `adr_pays` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+--
+-- Contenu de la table `Client`
+--
+
+INSERT INTO `Client` (`id_client`, `nom`, `prenom`, `identifiant`, `mot_de_passe`, `adr_numero`, `adr_voie`, `adr_code_postal`, `adr_ville`, `adr_pays`) VALUES
+(1, 'Dupont', 'Jean', 'jeandupont@gmail.com', 'Azerty1?', '56', 'Rue de la foret', '57000', 'METZ', 'FRANCE'),
+(2, 'Jean', 'Pierre', 'jeanpierre@gmail.com', 'Azerty1?', '5', 'Chemin des corses', '51000', 'REIMS', 'FRANCE');
 
 -- --------------------------------------------------------
 
@@ -70,7 +78,16 @@ CREATE TABLE IF NOT EXISTS `Commande` (
   `id_commande` int(5) NOT NULL,
   `date_commande` date NOT NULL,
   `id_client` int(5) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+--
+-- Contenu de la table `Commande`
+--
+
+INSERT INTO `Commande` (`id_commande`, `date_commande`, `id_client`) VALUES
+(1, '2021-01-22', 1),
+(2, '2020-12-09', 1),
+(3, '2021-01-23', 2);
 
 -- --------------------------------------------------------
 
@@ -82,6 +99,20 @@ CREATE TABLE IF NOT EXISTS `Favori` (
   `id_client` int(5) NOT NULL,
   `id_produit` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `Favori`
+--
+
+INSERT INTO `Favori` (`id_client`, `id_produit`) VALUES
+(1, 1),
+(1, 2),
+(1, 3),
+(1, 4),
+(1, 6),
+(1, 8),
+(2, 1),
+(2, 8);
 
 -- --------------------------------------------------------
 
@@ -97,6 +128,16 @@ CREATE TABLE IF NOT EXISTS `Ligne_commande` (
   `tarif` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Contenu de la table `Ligne_commande`
+--
+
+INSERT INTO `Ligne_commande` (`id_commande`, `id_produit`, `id_taille`, `quantite`, `tarif`) VALUES
+(1, 1, 1, 1, 52),
+(1, 2, 1, 2, 83),
+(2, 4, 2, 1, 35.2),
+(3, 9, 7, 1, 15);
+
 -- --------------------------------------------------------
 
 --
@@ -106,9 +147,17 @@ CREATE TABLE IF NOT EXISTS `Ligne_commande` (
 CREATE TABLE IF NOT EXISTS `Magasin` (
   `id_magasin` int(5) NOT NULL,
   `nom` varchar(50) NOT NULL,
-  `latitude` varchar(50) NOT NULL,
-  `longitude` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `latitude` double NOT NULL,
+  `longitude` double NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `Magasin`
+--
+
+INSERT INTO `Magasin` (`id_magasin`, `nom`, `latitude`, `longitude`) VALUES
+(1, 'Magasin de Metz', 49.119309, 6.175716),
+(2, 'Magasin de Nancy', 48.6833, 6.2);
 
 -- --------------------------------------------------------
 
@@ -119,6 +168,20 @@ CREATE TABLE IF NOT EXISTS `Magasin` (
 CREATE TABLE IF NOT EXISTS `Mentions_legales` (
   `mentions` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `Mentions_legales`
+--
+
+INSERT INTO `Mentions_legales` (`mentions`) VALUES
+('La boutique monPullMoche, portant le SIRET 840.489.398.00012, est une société de droit français enregistrée auprès du RCS. Cette société est propriétaire du nom de domaine www.monpullmoche.fr (ci-après, le « site »).'),
+('<h1>Coordonnées</h1>Adresse : 9, rue du vivier 57000 METZ<br>FRANCE<br>Téléphone : +33 3 83 85 96 75<br>Email : info@monpullmoche.fr'),
+('<h1>Conditions générales d''utilisation</h1>Nous mettons tout en œuvre pour assurer une mise à jour fiable et constante de l''application. Toutefois, des erreurs ou omissions peuvent survenir. L''utilisateur devra donc signaler toutes modifications du site qu''il jugerait utile.'),
+('<h1>Services fournis</h1>L''ensemble des activités de la société ainsi que ses informations sont présentés sur notre site www.monpullmoche.fr.<br><br>Nous nous efforçons de fournir des informations aussi précises que possibles. Les renseignements figurant sur le site et l''application mobile ne sont pas exhaustifs et les photos non contractuelles. Ils sont donnés sous réserve de modifications ayant été apportées depuis leur mise en ligne. Par ailleurs, toutes les informations indiquées sur le site ou l''application mobile sont données à titre indicatif, et sont susceptibles de changer ou d''évoluer sans préavis.<br><br>Les liens hypertextes mis en place dans le cadre du présent site en direction d''autres ressources présentes sur le réseau Internet ne sauraient engager la responsabilité de La boutique monpullmoche.'),
+('<h1>Propriété intellectuelle</h1>Tout le contenu du présent site www.monpullmoche.fr, incluant, de façon non limitative, les graphismes, images, textes, vidéos, animations, sons, logos, gifs et icônes ainsi que leur mise en forme sont la propriété exclusive de la société à l’exception des marques, logos ou contenus appartenant à d’autres sociétés partenaires ou auteurs. Toute reproduction, distribution, modification, adaptation, retransmission ou publication, même partielle, de ces différents éléments est strictement interdite sans l’accord exprès par écrit de La boutique monpullmoche. Cette représentation ou reproduction, par quelque procédé que ce soit, constitue une contrefaçon sanctionnée par les articles L.335–2 et suivants du Code de la propriété intellectuelle. Le non-respect de cette interdiction constitue une contrefaçon pouvant engager la responsabilité civile et pénale du contrefacteur. En outre, les propriétaires des Contenus copiés pourraient intenter une action en justice à votre encontre.'),
+('<h1>Déclaration à la CNIL</h1>Conformément à la loi 78–17 du 6 janvier 1978 (modifiée par la loi 2004–801 du 6 août 2004 relative à la protection des personnes physiques à l’égard des traitements de données à caractère personnel) relative à l’informatique, aux fichiers et aux libertés, ce site a fait l’objet d’une déclaration 1657682 auprès de la Commission nationale de l’informatique et des libertés (www.cnil.fr).'),
+('<h1>Litiges</h1>Les présentes conditions du site www.monpullmoche.fr sont régies par les lois françaises et toute contestation ou litiges qui pourraient naître de l’interprétation ou de l’exécution de celles-ci seront de la compétence exclusive des tribunaux dont dépend le siège social de la société. La langue de référence, pour le règlement de contentieux éventuels, est le français.'),
+('<h1>Données personnelles</h1>De manière générale, vous n’êtes pas tenu de nous communiquer vos données personnelles lorsque vous visitez notre site.<br><br>Cependant, ce principe comporte certaines exceptions. En effet, pour certains services proposés par notre site, vous pouvez être amenés à nous communiquer certaines données telles que : votre nom, votre adresse, votre adresse électronique...<br><br>Dans tous les cas, vous pouvez refuser de fournir vos données personnelles. Dans ce cas, vous ne pourrez pas utiliser les services du site.<br><br>Enfin, nous pouvons collecter de manière automatique certaines informations vous concernant lors d’une simple navigation sur notre site Internet, notamment : des informations concernant l’utilisation de notre site, comme les zones que vous visitez et les services auxquels vous accédez, votre adresse IP, le type de votre navigateur, vos temps d’accès.<br><br>De telles informations sont utilisées exclusivement à des fins de statistiques internes, de manière à améliorer la qualité des services qui vous sont proposés. Les bases de données sont protégées par les dispositions de la loi du 1er juillet 1998 transposant la directive 96/9 du 11 mars 1996 relative à la protection juridique des bases de données.');
 
 -- --------------------------------------------------------
 
@@ -140,7 +203,7 @@ CREATE TABLE IF NOT EXISTS `Produit` (
 --
 
 INSERT INTO `Produit` (`id_produit`, `titre`, `description`, `tarif`, `visuel`, `id_categorie`) VALUES
-(1, 'A Noël c''est moi qui tient les rennes BDD', 'Un pull qui ravira les petits et les grands. Tricoté par d''authentiques grand-mères Australiennes, en laine 100% coton naturel issue d''une filière agriculture durable certifiée ISO-2560.', 52, 'renne.jpg', 1),
+(1, 'A Noël c''est moi qui tient les rennes', 'Un pull qui ravira les petits et les grands. Tricoté par d''authentiques grand-mères Australiennes, en laine 100% coton naturel issue d''une filière agriculture durable certifiée ISO-2560.', 52, 'renne.jpg', 1),
 (2, 'Chien de noël', 'Un pull qui te permettra de faire baver d''envie tes petits camarades.', 41.5, 'chien.jpg', 1),
 (3, 'Un lama au poil !', 'Un grand classique revisité, c''est toi le plus beau quand tu es décoré par ce pull !', 44, 'lama.jpg', 1),
 (4, 'Mon beau sapin !', 'Ah, le chic à la Française, notre roi des forêts !', 35.2, 'sapin.jpg', 1),
@@ -307,17 +370,17 @@ ALTER TABLE `Categorie`
 -- AUTO_INCREMENT pour la table `Client`
 --
 ALTER TABLE `Client`
-  MODIFY `id_client` int(5) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_client` int(5) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT pour la table `Commande`
 --
 ALTER TABLE `Commande`
-  MODIFY `id_commande` int(5) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_commande` int(5) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT pour la table `Magasin`
 --
 ALTER TABLE `Magasin`
-  MODIFY `id_magasin` int(5) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_magasin` int(5) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT pour la table `Produit`
 --
